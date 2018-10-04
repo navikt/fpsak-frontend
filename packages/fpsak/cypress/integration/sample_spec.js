96616199ef6792ba307b2daf3cb94343ab0753e8
Michael Johansen <michael.johansen@nav.no>
2018-10-04 17:42:49 +0200

    Cypress: Happy Path fungerer frem til inntektsmelding.

--- packages/fpsak/cypress/integration/sample_spec.js
+++ packages/fpsak/cypress/integration/sample_spec.js
@@ -1,5 +1,6 @@
+const moment = require('moment');
 let environment;
-if (Cypress.env('baseUrl') === 'http://localhost:9000') {
+if (Cypress.config().baseUrl === 'http://localhost:9000') {
   environment = 'Lokal utvikling-Utbyggeren';
 } else {
   environment = 't10';
@@ -11,17 +12,21 @@ const foreldrepengesoknadUrl = `${testHubBase}/foreldrepengesoknad/sendviafordel
 const enkelKvinneSok = require('../test-data/person-sok/enkel-kvinne');
 const enkelForeldrepengeSoknad = require('../test-data/foreldrepengesoknad-sendviafordeling/enkel-soknad');
 
+let kjentFodselsnummer;
+
 const pollFagsakReady = function (fnr) {
   cy.request({
     url: 'http://localhost:8080/fpsak/api/fagsak/sok',
     method: 'POST',
+    headers: {
+      Accept: 'application/json',
+    },
     body: {
       searchString: fnr,
     },
   })
     .then((resp) => {
       if (resp.body.length > 0) {
-        console.log(resp);
       } else {
         console.log('Not ready, polling...');
         cy.wait(1000);
@@ -29,7 +34,30 @@ const pollFagsakReady = function (fnr) {
       }
     });
 };
+const formatDate = function formatDate(date) {
+  return moment(date)
+    .format('DD.MM.YYYY');
+};
 
+const createSoknadFromTestHub = function (personSokTemplate, soknadsTemplate) {
+  // henter ut en person
+  cy.request({
+    url: sokPersonUrl,
+    method: 'POST',
+    body: personSokTemplate,
+  })
+    .then((sokPersonResp) => {
+      soknadsTemplate.fnr = sokPersonResp.body.fnr;
+      cy.request({
+        url: foreldrepengesoknadUrl,
+        method: 'POST',
+        body: soknadsTemplate,
+      })
+        .then((result) => {
+          pollFagsakReady(soknadsTemplate.fnr);
+        });
+    });
+};
 describe('My First Cypress Test', () => {
   before(() => {
     // logger inn
@@ -38,24 +66,12 @@ describe('My First Cypress Test', () => {
       .then((resp) => {
         cy.setCookie('ID_token', resp.body.token);
         cy.setCookie('refresh_token', resp.body.refreshToken);
-      });
-    // henter ut en person
-    cy.request({
-      url: sokPersonUrl,
-      method: 'POST',
-      body: enkelKvinneSok,
-    })
-      .then((sokPersonResp) => {
-        enkelForeldrepengeSoknad.fnr = sokPersonResp.body.fnr;
-        cy.request({
-          url: foreldrepengesoknadUrl,
-          method: 'POST',
-          body: enkelForeldrepengeSoknad,
-        })
-          .then((result) => {
-            console.log(result);
-            // pollFagsakReady(enkelForeldrepengeSoknad.fnr)
-          });
+        if (!kjentFodselsnummer) {
+          createSoknadFromTestHub(enkelKvinneSok, enkelForeldrepengeSoknad);
+          kjentFodselsnummer = enkelForeldrepengeSoknad.fnr;
+        } else {
+          enkelForeldrepengeSoknad.fnr = kjentFodselsnummer;
+        }
       });
   });
 
@@ -67,5 +83,19 @@ describe('My First Cypress Test', () => {
     cy.visit('/');
     cy.get('input#searchString')
       .type(`${enkelForeldrepengeSoknad.fnr}{enter}`);
+    cy.wait(1000);
+    cy.url()
+      .should('include', 'fakta=foedselsvilkaaret');
+    cy.contains('Dokumentasjon foreligger')
+      .click();
+    cy.get('#antallBarnFodt')
+      .type(enkelForeldrepengeSoknad.antallBarn);
+
+    cy.get('input[placeholder="dd.mm.åååå"]')
+      .type(formatDate(enkelForeldrepengeSoknad.foedsel.foedselsdato));
+    cy.get('#begrunnelse')
+      .type('Manglet i testgrunnlaget... :-/');
+    cy.contains('Bekreft og fortsett')
+      .click();
   });
 });
