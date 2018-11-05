96ff86e90d25a04e0af225a2d3f446f0e559d698
Michael Johansen <michael.johansen@nav.no>
2018-11-05 15:45:03 +0100

    Openidc lagt til docker-oppsettet.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,35 @@
-FROM zzswang/docker-nginx-react:latest
+FROM openresty/openresty:alpine-fat
+LABEL maintainer="teamforeldrepenger"
+
 COPY ./dist /app
+
+RUN apk add --no-cache bash gettext libintl
+
+RUN ["luarocks", "install", "lua-resty-session"]
+RUN ["luarocks", "install", "lua-resty-http"]
+RUN ["luarocks", "install", "lua-resty-jwt"]
+RUN ["luarocks", "install", "lua-resty-openidc"]
+
+COPY docker/openidc.lua /usr/local/openresty/lualib/resty/
+
 ENV DEBUG=on \
-    APP_API_PLACEHOLDER="/fpsak" \
-    CLIENT_HEADER_TIMEOUT=5 \
-    CLIENT_BODY_TIMEOUT=5
-COPY docker/nginx-site.conf /etc/nginx/conf.d/app.conf.template
-EXPOSE 9000
+	APP_DIR=/app \
+	APP_PATH_PREFIX=/aSubSiteInParentDomainUseThisPath \
+	APP_API_PLACEHOLDER="/fpsak" \
+	APP_API_GATEWAY="" \
+	CLIENT_BODY_TIMEOUT=10 \
+	CLIENT_HEADER_TIMEOUT=10 \
+	CLIENT_MAX_BODY_SIZE=1024 \
+	WHITE_LIST_IP=(172.17.0.1)|(192.168.0.25) \
+	WHITE_LIST=off
+
+COPY docker/default-config.nginx /etc/nginx/conf.d/app.conf.template
+COPY docker/start-nginx.sh /usr/sbin/start
+
+RUN chmod u+x /usr/sbin/start
+
+EXPOSE 8080
+
+WORKDIR ${APP_DIR}
+
+CMD ["start"]
--- docker/start-nginx.sh
+++ docker/start-nginx.sh
@@ -1,26 +1,18 @@
 #!/bin/sh
 
-echo "setting nginx conf ..."
-echo "DEBUG": $DEBUG
-echo "APP_DIR": $APP_DIR
-echo "APP_PATH_PREFIX": $APP_PATH_PREFIX
-echo "APP_API_PLACEHOLDER": $APP_API_PLACEHOLDER
-echo "APP_API_GATEWAY": $APP_API_GATEWAY
-echo "CLIENT_BODY_TIMEOUT": $CLIENT_BODY_TIMEOUT
-echo "CLIENT_HEADER_TIMEOUT": $CLIENT_HEADER_TIMEOUT
-echo "CLIENT_MAX_BODY_SIZE": $CLIENT_MAX_BODY_SIZE
+# Picking the first nameserver.
+export RESOLVER=$(cat /etc/resolv.conf | grep -v '^#' | grep -m 1 nameserver | awk '{print $2}')
+echo "Will use resolver:" $RESOLVER
 
 # replace env for nginx conf
-envsubst '$WHITE_LIST $WHITE_LIST_IP $DEBUG $APP_DIR $APP_PATH_PREFIX $APP_API_PLACEHOLDER $APP_API_GATEWAY $CLIENT_BODY_TIMEOUT $CLIENT_HEADER_TIMEOUT $CLIENT_MAX_BODY_SIZE' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$WHITE_LIST $WHITE_LIST_IP $DEBUG $APP_DIR $APP_PATH_PREFIX $APP_API_PLACEHOLDER $APP_API_GATEWAY $CLIENT_BODY_TIMEOUT $CLIENT_HEADER_TIMEOUT $CLIENT_MAX_BODY_SIZE $OPENID_USERNAME $OPENID_PASSWORD $OPENID_ISSOHOST $RESOLVER $APP_NAME $APP_VERSION $FASIT_ENVIRONMENT_NAME' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/fpsak.conf
 
 # find all env start with APP_
 export SUBS=$(echo $(env | cut -d= -f1 | grep "^APP_" | sed -e 's/^/\$/'))
 
 # replace above envs
-echo "inject environments ..."
-echo $SUBS
-for f in `find /usr/share/nginx/html -regex ".*\.\(js\|css\|html\|json\|map\)"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
+echo "inject envs: " $SUBS
+for f in `find /$APP_DIR -regex ".*\.\(js\|css\|html\|json\|map\)"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
 
-# start nginx
-nginx -g 'daemon off;'
+/usr/local/openresty/bin/openresty -g 'daemon off;'
 exec "$@"
