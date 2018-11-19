4d89cc611677502f107e29c8278774fdaa7eaf4d
Michael Johansen <michael.johansen@nav.no>
2018-11-19 16:44:22 +0100

    Added version bumper for travis.

---
+++ _scripts/bump.sh
@@ -0,0 +1,7 @@
+
+PUSH_URL="https://x-access-token:$INSTALLATION_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "master" ]]; then
+    yarn version --patch
+    git push "${PUSH_URL}" --tags >/dev/null 2>&1
+fi
