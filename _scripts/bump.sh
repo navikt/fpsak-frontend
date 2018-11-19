b4412fc86a896f061781c6758a23d1c034f6bb30
Michael Johansen <michael.johansen@nav.no>
2018-11-19 17:02:53 +0100

    La til migreringsscript for å flytte fra stash.

--- _scripts/bump.sh
+++ _scripts/bump.sh
@@ -1,7 +1,10 @@
+#!/bin/bash -e
 
 PUSH_URL="https://x-access-token:$INSTALLATION_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "master" ]]; then
     yarn version --patch
     git push "${PUSH_URL}" --tags >/dev/null 2>&1
+else
+    echo "Will not bump version on branch ${TRAVIS_BRANCH} (Pull request: ${TRAVIS_PULL_REQUEST})"
 fi
