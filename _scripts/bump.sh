3610ff5c86192d0d597eada53a944bcfc87bd856
mijohansen <michael.johansen@nav.no>
2018-11-19 19:00:48 +0100

    prøver med færre flagg

--- _scripts/bump.sh
+++ _scripts/bump.sh
@@ -3,7 +3,7 @@
 PUSH_URL="https://x-access-token:$INSTALLATION_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "master" ]]; then
-    yarn version --new-version patch --silent --no-commit-hooks --non-interactive --no-node-version-check --no-progress --emoji false
+    yarn version --new-version patch --non-interactive
     git push "${PUSH_URL}" --tags >/dev/null 2>&1
 else
     echo "Will not bump version on branch ${TRAVIS_BRANCH} (Pull request: ${TRAVIS_PULL_REQUEST})"
