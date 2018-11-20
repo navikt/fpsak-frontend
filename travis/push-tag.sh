02069ffdb7824f6a8c243f0a2c62b4359acc7ce8
mijohansen <michael.johansen@nav.no>
2018-11-20 10:12:10 +0100

    Creating installation_tokens on the fly

--- travis/push-tag.sh
+++ travis/push-tag.sh
@@ -4,8 +4,7 @@ PUSH_URL="https://x-access-token:$INSTALLATION_TOKEN@github.com/$TRAVIS_REPO_SLU
 
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "master" ]]; then
     yarn version --new-version patch --non-interactive
-    git push --tags ${PUSH_URL} HEAD:master
-    #>/dev/null 2>&1
+    git push --tags ${PUSH_URL} HEAD:master >/dev/null 2>&1
     echo "Pushing the new version to $TRAVIS_REPO_SLUG"
 else
     echo "Will not bump version on branch ${TRAVIS_BRANCH} (Pull request: ${TRAVIS_PULL_REQUEST})"
