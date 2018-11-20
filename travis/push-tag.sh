b86420e4cf90612ca11fd966b9c16f1d91e445d5
Michael Johansen <michael.johansen@nav.no>
2018-11-20 15:06:09 +0100

    trying to fix update loop again

--- travis/push-tag.sh
+++ travis/push-tag.sh
@@ -2,7 +2,7 @@
 
 PUSH_URL="https://x-access-token:$INSTALLATION_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 # If this is not a pull_request, the branch is master and its not tagged. Then create a new tag.
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ -z "${TRAVIS_TAG}" ]]; then
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ "${TRAVIS_TAG}" = "" ]]; then
     yarn version --new-version patch --non-interactive
     git push --tags ${PUSH_URL} HEAD:master >/dev/null 2>&1
     echo "Pushing the new version to $TRAVIS_REPO_SLUG"
