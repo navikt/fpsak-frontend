b907d1d3a42ab3243c1b5996b6b452ffd256a34a
Michael Johansen <michael.johansen@gmail.com>
2018-11-28 12:51:04 +0100

    fixed_deatached_head_error (#26)

--- travis/push-tag.sh
+++ travis/push-tag.sh
@@ -5,6 +5,7 @@ GIT_USERNAME="$(git config user.name)"
 PUSH_URL="https://x-access-token:$INSTALLATION_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 # If this is not a pull_request, the branch is master and its not tagged. Then create a new tag.
 if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ "${AUTHOR_NAME}" != "${GIT_USERNAME}" ]]; then
+    git checkout master # switch to master branch (which is the same as the current commit), so we are not in DETACHED mode
     yarn version --new-version patch --non-interactive
     git push --tags ${PUSH_URL} HEAD:master >/dev/null 2>&1
     echo "Pushing the new version to $TRAVIS_REPO_SLUG as $GIT_USERNAME"
