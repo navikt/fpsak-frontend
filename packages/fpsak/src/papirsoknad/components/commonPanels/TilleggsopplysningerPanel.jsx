e567256d35a30df7f8dbe6ba4b74ecc1aa29fd55
Andreas Ahlgren <andreas.ahlgren@nav.no>
2018-11-30 13:19:50 +0100

    PFP-1610 - sprakstøtte med lokal feature toggle

--- packages/fpsak/src/papirsoknad/components/commonPanels/TilleggsopplysningerPanel.jsx
+++ packages/fpsak/src/papirsoknad/components/commonPanels/TilleggsopplysningerPanel.jsx
@@ -4,13 +4,22 @@ import { injectIntl, intlShape } from 'react-intl';
 import { Fieldset } from 'nav-frontend-skjema';
 
 import BorderBox from 'sharedComponents/BorderBox';
-import { TextAreaField } from 'form/Fields';
-import { maxLength as maxLengthValidator, hasValidText } from 'utils/validation/validators';
+import { TextAreaField, RadioGroupField, RadioOption } from 'form/Fields';
+import { maxLength as maxLengthValidator, hasValidText, required } from 'utils/validation/validators';
+import { Row, Column } from 'nav-frontend-grid';
 
 import styles from './tilleggsopplysningerPanel.less';
 
 const maxLength = 1500;
 const validate = [maxLengthValidator(maxLength), hasValidText];
+
+const localfeatureToggle = false;
+
+const sprakvalg = {
+  BOKMAL: 'BOKMAL',
+  NYNORSK: 'NYNORSK',
+  ENGELSK: 'ENGELSK',
+};
 
 /**
  * s
@@ -24,16 +33,45 @@ export const TilleggsopplysningerPanel = ({
 }) => (
   <BorderBox>
     <div className={styles.flexContainer}>
-      <Fieldset legend={intl.formatMessage({ id: 'Registrering.SokerensTilleggsopplysninger' })}>
-        <TextAreaField
-          name="tilleggsopplysninger"
-          label=""
-          textareaClass={styles.textAreaSettings}
-          maxLength={maxLength}
-          validate={validate}
-          readOnly={readOnly}
-        />
-      </Fieldset>
+      <Row>
+        <Column xs="6">
+          <Fieldset legend={intl.formatMessage({ id: 'Registrering.SokerensTilleggsopplysninger' })}>
+            <TextAreaField
+              name="tilleggsopplysninger"
+              label=""
+              textareaClass={styles.textAreaSettings}
+              maxLength={maxLength}
+              validate={validate}
+              readOnly={readOnly}
+            />
+          </Fieldset>
+        </Column>
+        {localfeatureToggle &&
+        <Column xs="6">
+          <Fieldset legend={intl.formatMessage({ id: 'Registrering.Sprak' })}>
+            <RadioGroupField
+              name="sprakvalg"
+              direction="vertical"
+              readOnly={readOnly}
+              validate={required}
+            >
+              <RadioOption
+                label={intl.formatMessage({ id: 'Registrering.Sprak.Bokmal' })}
+                value={sprakvalg.BOKMAL}
+              />
+              <RadioOption
+                label={intl.formatMessage({ id: 'Registrering.Sprak.Nynorsk' })}
+                value={sprakvalg.NYNORSK}
+              />
+              <RadioOption
+                label={intl.formatMessage({ id: 'Registrering.Sprak.Engelsk' })}
+                value={sprakvalg.ENGELSK}
+              />
+            </RadioGroupField>
+          </Fieldset>
+        </Column>
+        }
+      </Row>
     </div>
   </BorderBox>
 );
