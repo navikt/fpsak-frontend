8b7b11aae9a23b844db96067a5984c046feec9de
Petter <petterruud@gmail.com>
2018-12-07 08:51:39 +0100

    NOJIRA lagt til versjonsnummer på frontend i kildekoden (#80)

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,6 +2,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
+const PACKAGE = require('./../package.json');
+const VERSION = PACKAGE.version;
+
 const commonDevAndProd = require('./webpack.common.dev_and_prod.js');
 
 const ROOT_DIR = path.resolve(__dirname, '../public/client');
@@ -27,6 +30,8 @@ const config = {
       filename: 'index.html',
       favicon: path.join(ROOT_DIR, 'favicon.ico'),
       template: path.join(ROOT_DIR, 'index.html'),
+      version: VERSION,
+
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -6,6 +6,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const merge = require('webpack-merge');
 const commonDevAndProd = require('./webpack.common.dev_and_prod.js');
+const PACKAGE = require('./../package.json');
+const VERSION = PACKAGE.version;
 
 const ROOT_DIR = path.resolve(__dirname, '../public/client');
 const PACKAGES_DIR = path.resolve(__dirname, '../packages');
@@ -32,6 +34,7 @@ const config = {
       filename: '../index.html',
       favicon: path.join(ROOT_DIR, 'favicon.ico'),
       template: path.join(ROOT_DIR, 'index.html'),
+      version: VERSION,
     }),
   ],
   optimization: {
