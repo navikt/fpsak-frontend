dd73580c6f0478827adbd9ac8702d00411f1d34f
Petter <petterruud@gmail.com>
2019-01-28 11:17:02 +0100

    NOJIRA legger til sentry (#216)

--- packages/fpsak/src/index.jsx
+++ packages/fpsak/src/index.jsx
@@ -3,12 +3,18 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import { render } from 'react-dom';
+import { init } from '@sentry/browser';
 
 import errorHandler from '@fpsak-frontend/error-api-redux';
 
 import AppIndex from 'app/AppIndex';
 import configureStore from './store';
 import reducerRegistry from './ReducerRegistry';
+
+init({
+  dsn: 'https://b7a25f545630433aac46282548140778@sentry.nais.preprod.local/2',
+});
+
 
 const history = createBrowserHistory({
   basename: '/fpsak/',
