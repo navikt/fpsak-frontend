1d9f4ab5067478a6f6b4a6f9a2da1abaafe3e25a
markodraks <43438866+markodraks@users.noreply.github.com>
2019-01-30 15:58:54 +0100

    FIX behandlingsprosess simulering revurdering (#214)
    * FIX behandlingsprosess simulering revurdering
    
    * PFP-5307 fix simulering behandlingspunkt icon
    
    * NOJIRA fixing sonar issues, and removing unused simuleringresultat from bpbuilder
    
    * NOJIRA sonar fix

--- packages/fpsak/src/behandlingFpsak/behandlingsprosess/definition/simuleringStatusUtleder.jsx
+++ packages/fpsak/src/behandlingFpsak/behandlingsprosess/definition/simuleringStatusUtleder.jsx
@@ -1,10 +1,14 @@
 import vut from '@fpsak-frontend/kodeverk/src/vilkarUtfallType';
+import { isInnvilget, isAvslag } from '@fpsak-frontend/kodeverk/src/behandlingResultatType';
 import featureToggle from 'app/featureToggle';
 
 export const hasSimuleringOn = ({ featureToggles }) => featureToggles[featureToggle.SIMULER_OPPDRAG];
-export const getStatusFromSimulering = ({ resultatstruktur }) => {
-  if (!resultatstruktur) {
-    return vut.IKKE_VURDERT;
+export const getStatusFromSimulering = ({ resultatstruktur, behandlingsresultat }) => {
+  if (isInnvilget(behandlingsresultat.type.kode) || isAvslag(behandlingsresultat.type.kode)) {
+    return vut.OPPFYLT;
   }
-  return vut.OPPFYLT;
+  if (resultatstruktur) {
+    return vut.OPPFYLT;
+  }
+  return vut.IKKE_VURDERT;
 };
