c307659b285f92350eb89749d4360e981bab1758
Ingridcst <43202464+Ingridcst@users.noreply.github.com>
2019-02-21 13:37:18 +0100

    PFP-5917 Legger til manglende tekst på totrinns (#322)

--- packages/fpsak/src/behandlingsupport/approval/totrinnskontrollaksjonspunktTextCodes.jsx
+++ packages/fpsak/src/behandlingsupport/approval/totrinnskontrollaksjonspunktTextCodes.jsx
@@ -59,6 +59,7 @@ const totrinnskontrollaksjonspunktTextCodes = {
   [aksjonspunktCodes.KONTROLLER_TILSTØTENDE_YTELSER_INNVILGET]: 'ToTrinnsForm.Uttak.TilstotendeYtelser.Innvilget',
   [aksjonspunktCodes.KONTROLLER_TILSTØTENDE_YTELSER_OPPHØRT]: 'ToTrinnsForm.Uttak.TilstotendeYtelser.Opphort',
   [aksjonspunktCodes.TILKNYTTET_STORTINGET]: 'ToTrinnsForm.Uttak.Stortinget',
+  [aksjonspunktCodes.OVERSTYR_AVKLAR_FAKTA_UTTAK]: 'ToTrinnsForm.Uttak.Overstyrt',
 };
 
 export default totrinnskontrollaksjonspunktTextCodes;
