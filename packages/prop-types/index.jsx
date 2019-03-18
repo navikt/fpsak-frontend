aa599bde317a8cbd35b0158b4580c58ae7ca9f31
Petter <petterruud@gmail.com>
2019-03-18 09:47:17 +0100

    NOJIRA fjerner ubruke prop-types (#405)

--- packages/prop-types/index.jsx
+++ packages/prop-types/index.jsx
@@ -6,8 +6,6 @@ export {
   arbeidsgiverUttakPropType,
   arbeidsforholdBeregningProptype,
 } from './src/arbeidsforholdPropType';
-export { default as behandlingPropType } from './src/behandlingPropType';
-export { default as behandlingsresultatPropType } from './src/behandlingsresultatPropType';
 export {
   uttakPropType,
   beregningsresultatPeriodeAndelPropType,
@@ -16,16 +14,12 @@ export {
 } from './src/beregningresultatMedUttaksplanPropType';
 export { default as beregningsgrunnlagPropType } from './src/beregningsgrunnlagPropType';
 export { default as fagsakPropType } from './src/fagsakPropType';
-export { default as familiehendelsePropType } from './src/familiehendelsePropType';
 export { default as historikkinnslagDelPropType } from './src/historikkinnslagDelPropType';
 export { default as historikkinnslagPropType } from './src/historikkinnslagPropType';
 export { kodeverkPropType, kodeverkObjektPropType } from './src/kodeverkPropType';
-export { default as medlemPropType } from './src/medlemPropType';
 export { default as navAnsattPropType } from './src/navAnsattPropType';
-export { default as opptjeningPropType } from './src/opptjeningPropType';
 export { default as personopplysningPropType } from './src/personopplysningPropType';
 export { default as rettighetPropType } from './src/rettighetPropType';
-export { default as soknadPropType } from './src/soknadPropType';
 export {
   stonadskontoPropType,
   stonadskontoerPropType,
