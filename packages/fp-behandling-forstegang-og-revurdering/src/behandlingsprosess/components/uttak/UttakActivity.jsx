3326107ad368afce2e78924ac09eddd7d6d08083
Petter Ruud <petter.ruud@nav.no>
2019-05-22 16:34:31 +0200

    FIX desimaler

--- packages/fp-behandling-forstegang-og-revurdering/src/behandlingsprosess/components/uttak/UttakActivity.jsx
+++ packages/fp-behandling-forstegang-og-revurdering/src/behandlingsprosess/components/uttak/UttakActivity.jsx
@@ -422,7 +422,7 @@ const calculateCorrectDays = (aktivitet, item) => {
   if (aktivitet.trekkdagerDesimaler && aktivitet.trekkdagerDesimaler < 0) {
     return 0;
   }
-  return Math.floor(aktivitet.trekkdager % 5);
+  return Math.floor(aktivitet.trekkdager % 5).toFixed(1);
 };
 
 export const initialValue = (selectedItem, kontoIkkeSatt) => {
