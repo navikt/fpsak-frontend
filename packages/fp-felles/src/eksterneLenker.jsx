03c4a40fb9dc4bf7de29cd088c00e0e1f50aefa1
Michael Johansen <michael.johansen@nav.no>
2019-06-26 10:30:14 +0200

    Update eksterneLenker.jsx (#681)

--- packages/fp-felles/src/eksterneLenker.jsx
+++ packages/fp-felles/src/eksterneLenker.jsx
@@ -1,4 +1,4 @@
-export const RETTSKILDE_URL = 'https://www.nav.no/rettskildene-intern/';
+export const RETTSKILDE_URL = 'https://lovdata.no/pro/sso/login/nav';
 export const LINK_TIL_BESTE_BEREGNING_REGNEARK = 'https://navno.sharepoint.com/sites/fag-og-ytelser-familie-foreldrepenger';
 /* eslint-disable max-len */
 export const SYSTEMRUTINE_URL = 'https://navno.sharepoint.com/sites/fag-og-ytelser-fagsystemer/SitePages/Saksbehandlingsl%C3%B8sningen-for-foreldrepenger-og-engangst%C3%B8nad.aspx';
