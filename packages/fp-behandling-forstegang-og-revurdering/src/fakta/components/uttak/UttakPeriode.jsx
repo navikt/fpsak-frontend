80fe919099f6ecb51f0cc822cf809f67f985aa36
Michael Johansen <michael.johansen@nav.no>
2019-09-11 12:29:40 +0200

    TFP-1108 Error i fakta om uttak (#787)

--- packages/fp-behandling-forstegang-og-revurdering/src/fakta/components/uttak/UttakPeriode.jsx
+++ packages/fp-behandling-forstegang-og-revurdering/src/fakta/components/uttak/UttakPeriode.jsx
@@ -133,7 +133,7 @@ const UttakPeriode = ({
                   />
                 </div>
                 {perioder.length === fields.length
-                  && renderValidationGraphic(perioder, index, index === (fields.length - 1))}
+                && renderValidationGraphic(perioder, index, index === (fields.length - 1), intl)}
               </FlexColumn>
             </FlexRow>
           </React.Fragment>
