c01458f220a0cb85f2ed0e67cefd2b35c08815ee
Tor Røneid <tor.roneid@nav.no>
2019-09-17 08:12:28 +0200

    NOJIRA lagt til react-storybook (#793)

---
+++ packages/storybook/stories/sharedComponents.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { createIntl, createIntlCache, RawIntlProvider } from 'react-intl';
+
+import { storiesOf } from '@storybook/react';
+import { intlMock } from '@fpsak-frontend/utils-test/src/intl-enzyme-test-helper';
+import { OkAvbrytModal, DateLabel, ArrowBox } from '@fpsak-frontend/shared-components';
+
+const intl = createIntl({
+  locale: 'nb-NO',
+  messages: {
+    'OkAvbrytModal.Ok': 'Ok',
+    'OkAvbrytModal.Avbryt': 'Avbryt',
+    'Test.Test': 'Dette er ein test',
+  },
+}, createIntlCache());
+
+storiesOf('OkAvbrytModal', module)
+  .add('vis modal', () => (
+    <RawIntlProvider value={intl}>
+      <OkAvbrytModal
+        intl={intlMock}
+        textCode="Test.Test"
+        showModal
+        submit={() => alert('ok')}
+        cancel={() => alert('avbryt')}
+      />
+    </RawIntlProvider>
+  ));
+
+storiesOf('DateLabel', module)
+  .add('vis formatert dato', () => (
+    <RawIntlProvider value={intl}>
+      <DateLabel
+        dateString="2017-10-02"
+      />
+    </RawIntlProvider>
+  ));
+
+storiesOf('ArrowBox', module)
+  .add('med pil på toppen', () => (
+    <div style={{ width: '200px', marginTop: '20px' }}>
+      <ArrowBox>Dette er en tekst</ArrowBox>
+    </div>
+  ));
